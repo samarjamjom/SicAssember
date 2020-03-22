1ff441f54157ceffb2d19ce59f190aeb8917c655
samar jamjom <samarjamjom@gmail.com>
2020-03-22 20:11:43 +0200

    second v

--- pass1.py
+++ pass1.py
@@ -1,96 +1,125 @@
 """ The output of Pass 1 is:
 1. Symbol Table SYBTAB: should be displayed on the screen.
 2. LOCCTR, PRGLTH, PRGNAME, ...
-3. Intermediate file (.mdt): Stored on the secondary storage. """
+3. Intermediate file (.mdt): Stored on the secondary storage. 
+4.Your assembler can stop execution if there are errors in Pass 1"""
+import codecs
 
 #open file to read it
 sic_source_file = open("source.txt", "r")
 
+#open file to read it
+opcode_table_file = open("OPTAB.txt", "r")
+
 #read  all input lines
 sic_assembly = sic_source_file.readlines()
 
+#read opcode table 
+opcode_table = opcode_table_file.readlines()
+
+
 #initialize symbol table, program name, program length and locctr
-symbol_table = [["label1", 1], ["label2",2]]
-opt_table = []
+symbol_table = {}
+opt_table = {}
 prog_name = ""
 prog_leng = 0
-loc_ctr 
+start_add = 0
 
-#initialize instruction comp
-label
-opcode
-operand
-comment
+#initialize instruction component
+label = ""
+opcode = ""
+operand = 0
+comment = ""
+counter = 0
 
 #create a .text files  
-intermid_file = open("intermid.txt","w+")
+intermid_file = open("intermid.mdt","a+")
 
 #initialize a list of directives
 directives = ["START", "END", "BYTE", "WORD", "RESB", "RESW", "BASE", "LTORG"]
 
+#store opcode table in 2D list
+for ind, line in enumerate(opcode_table):
+    #read file from third line 
+    if ind>1:
+        opt_table[line[0:11].split(' ')[0]] = line[15:18].strip()
+
+
+#read first input line
+first_line = sic_assembly[0]
+if first_line[9:15].strip() == "START":
+    prog_name =  first_line[0:8].strip()
+    start_add = int(first_line[16:35].strip(),16)
+    loc_ctr = start_add
+    #to save a fixed format for intermediate file 
+    blanks = 6-len(str((loc_ctr)))
+    #write line to intemediate file
+    intermid_file.write(hex(loc_ctr)[2:]+" "*blanks+first_line)
+    
+else:
+    loc_ctr = 0
 
 for ind, line in enumerate(sic_assembly):
-    #read first input line
-    if ind == 0 and line[9:15].strip() == "START":
-        prog_name = line[0:8].strip()
-        loc_ctr = line[9:15].strip()
-        intermid_file.write(loc_ctr+" "+line)
-    else:
-        loc_ctr = 0
+    #read opcode
+    opcode = line[9:15].strip()
+    if opcode != "END" and opcode != "START" and opcode != "BASE":
+        #if this is not a comment line
+        if line[0] != '.':
+            #read label field
+            label = line[0:8].strip()
 
-    #if this is not a comment line
-    if line[0] != '.':
-
-        #read label field
-        label = line[0:8]
-        #if there is asymbol in label field
-        if label != "":
-            #serch SYMTAB for LABEL
-            for j in len(symbol_table):
+            #if there is asymbol in label field
+            if label != "":
+                #serch SYMTAB for LABEL
                 #if found
-                if symbol_table[j][0]==label:
+                if label in symbol_table:
                     #set error flag
                     print("ERROR, Duplicate symbol!")
                     break
-            #insert [label, LOCCTR] into SYMTAB
-            symbol_table.append([label,loc_ctr])
+                #else insert [label, LOCCTR] into SYMTAB
+                else:
+                    symbol_table[label] = hex(loc_ctr)[2:]
 
-        #read opcode
-        found = 0
-        opcode = line[9:15].strip()
-        #search OPTAB for OPCODE
-        for j in len(opt_table):
-            #if found
-            if opt_table[j]==opcode:
+            #found is a boolean to  determine if opcode exist in opcode table
+            found = 0
+
+            #search OPTAB for OPCODE
+            if opcode in opt_table:
+                #if found
                 found = 1
                 #add 3 {instruction length} to LOCCTR
                 loc_ctr += 3
-            if found = 0:
+
+            if found == 0 and opcode in directives:
                 if opcode == "WORD":
+                    #add 3 {instruction length}
                     loc_ctr += 3
                 elif opcode == "RESW":
-                    operand = int(line[17:35].strip())
-                    loc_ctr += 3*operand
+                    operand = line[16:35].strip()
+                    loc_ctr += 3 * int(operand)
+                elif opcode == "RESB":
+                    operand = line[16:35].strip()
+                    loc_ctr += int(operand)
+                elif opcode == "BYTE":
+                    operand = line[16:35].strip()
+                    #find the length of constant in bytes and add it to loc_ctr
+                    if operand[0] == 'X':
+                        loc_ctr += int((len(operand) - 3)/2)
+                    elif operand[0] == 'C':
+                        loc_ctr += (len(operand) - 3)
+            #write line to intemediate file
+            print(hex(loc_ctr)[2:] +""+ line)
+            intermid_file.write(hex(loc_ctr)[2:] +"  "+ line)
 
-
-
-    
-
-
-
-
-        #list of operands
-        #if len(line) >= 17:
-    
-        #list of comments
-        #if len(line) >= 35:
-            #comments.append(line[35:66].strip())
+#save (loc_ctr - starting add ) as program length
+prog_leng = int(loc_ctr) - int(start_add)
 
 #close file
 sic_source_file.close()
+opcode_table_file.close()
+intermid_file.close()
 
-print("name of the program: ",name_prog)
-
-
-
-
+print("name of the program: ",prog_name)
+print("length of the program: ",hex(int(prog_leng)).format(int(prog_leng)))
+print("LOCCTR: ",hex(int(loc_ctr)).format(int(loc_ctr)))
+print("symbol table : ",symbol_table)
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
+h = int("a", 16)
+f = int("4", 16)
+jh = h + f
 
+print(hex(h).split("0x"))
