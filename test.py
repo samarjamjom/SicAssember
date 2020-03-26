839308184d9da1cad3d5654a06584d00dc0348f4
Latifa <42656449+Latifa-Masri@users.noreply.github.com>
2020-03-26 22:41:39 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-h = int("a", 16)
-f = int("4", 16)
-jh = h + f
+# h = int("a", 16)
+# f = int("4", 16)
+# jh = h + f
 
-print(hex(h).split("0x"))
+# print(hex(h).split("0x"))
+
+
+c = '454f46' # for example 
+s = c.encode("utf-8").hex()
+print(s)
